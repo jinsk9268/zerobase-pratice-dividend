c8d46e5497bb3dd48c325b25b27838e83a1f919f
jinsk9268 <jinsk9268@gmail.com>
2023-03-22 11:21:40 +0900

    feat: ScrapedResult 모델 클래스 생성

---
+++ src/main/java/com/zerobase/dividend/model/ScrapedResult.java
@@ -0,0 +1,24 @@
+package com.zerobase.dividend.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 스크래핑한 결과를 주고받기 위한 result class
+ * 스크래핑한 회사의 정보를 가진 Company Instance
+ * 배당금 정보를 가진 Dividend List
+ * 한 회사는 여러가지 배당금 정보를 가지기 때문
+ */
+@Data
+@AllArgsConstructor // 모든 필드를 초기화하는 생성자 코드를 사용할 수 있는 어노테이션
+public class ScrapedResult {
+    private Company company;
+    private List<Dividend> dividendEntities;
+
+    public ScrapedResult() {
+        this.dividendEntities = new ArrayList<>();
+    }
+}
