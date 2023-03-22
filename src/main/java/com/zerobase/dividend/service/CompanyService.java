d4b07ca693c9101ba796009060c2de8feebb9992
jinsk9268 <jinsk9268@gmail.com>
2023-03-22 16:42:05 +0900

    feat: CompanyService-회사, 배당금 정보 저장 메서드 기능 추가
    - storeCompanyAndDividend

--- src/main/java/com/zerobase/dividend/service/CompanyService.java
+++ src/main/java/com/zerobase/dividend/service/CompanyService.java
@@ -2,19 +2,31 @@ package com.zerobase.dividend.service;
 
 import com.zerobase.dividend.model.Company;
 import com.zerobase.dividend.model.ScrapedResult;
+import com.zerobase.dividend.persist.CompanyRepository;
+import com.zerobase.dividend.persist.DividendRepository;
+import com.zerobase.dividend.persist.entity.CompanyEntity;
+import com.zerobase.dividend.persist.entity.DividendEntity;
 import com.zerobase.dividend.scraper.Scraper;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @AllArgsConstructor // Bean이 생성될 때 사용할 수 있도록
 public class CompanyService {
     private final Scraper yahooFiananceScraper;
+
+    private final CompanyRepository companyRepository;
+    private final DividendRepository dividendRepository;
+
     public Company save(String ticker) {
         return null;
     }
 
+    // 클래서 밖에선 메서드 호출할 수 없음
     private Company storeCompanyAndDividend(String ticker) {
         // ticker를 기준으로 회사를 스크래핑
         Company company = this.yahooFiananceScraper.scrapCompanyByTicker(ticker);
@@ -28,7 +40,16 @@ public class CompanyService {
         ScrapedResult scrapedResult = this.yahooFiananceScraper.scrap(company);
 
         // 스크래핑 결과
+        CompanyEntity companyEntity = this.companyRepository.save(new CompanyEntity(company));
+        // .map - Collection의 element를 다른값으로 맵핑해야될때 사용
+        // Dividend 인스턴스 하나하나가 e에 해당
+        List<DividendEntity> dividendEntities = scrapedResult.getDividends().stream()
+                .map(e -> new DividendEntity(companyEntity.getId(), e))
+                .collect(Collectors.toList());
 
-        return null;
+        // 다건 데이터 저장이므로 saveAll
+        this.dividendRepository.saveAll(dividendEntities);
+
+        return company;
     }
 }
