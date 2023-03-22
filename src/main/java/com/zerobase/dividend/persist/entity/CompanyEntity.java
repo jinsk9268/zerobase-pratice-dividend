04b86fd2185d57edc28962c44c4acfd5fec49a1f
jinsk9268 <jinsk9268@gmail.com>
2023-03-22 16:04:27 +0900

    feat: CompanyEntity-DB 저장을 쉽게 하기 위한 생성자 추가

--- src/main/java/com/zerobase/dividend/persist/entity/CompanyEntity.java
+++ src/main/java/com/zerobase/dividend/persist/entity/CompanyEntity.java
@@ -1,5 +1,6 @@
 package com.zerobase.dividend.persist.entity;
 
+import com.zerobase.dividend.model.Company;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
@@ -17,4 +18,10 @@ public class CompanyEntity {
     @Column(unique = true) // ticker 중복되면 안되므로 unique 설정
     private String ticker;
     private String name;
+
+    // DB 저장을 쉽게 하기 위한 생성자 생성
+    public CompanyEntity(Company company) {
+        this.ticker = company.getTicker();
+        this.name = company.getName();
+    }
 }
