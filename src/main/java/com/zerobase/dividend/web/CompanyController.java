2a116261d80ab376cc12c9a8b3a6fdd91ee181ef
jinsk9268 <jinsk9268@gmail.com>
2023-03-22 21:15:25 +0900

    feat: CompanyController-addCompany API POST 요청 완성

--- src/main/java/com/zerobase/dividend/web/CompanyController.java
+++ src/main/java/com/zerobase/dividend/web/CompanyController.java
@@ -1,11 +1,18 @@
 package com.zerobase.dividend.web;
 
+import com.zerobase.dividend.model.Company;
+import com.zerobase.dividend.service.CompanyService;
+import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/company") // 공통경로 설정
+@AllArgsConstructor
 public class CompanyController {
+    private final CompanyService companyService;
+
     @GetMapping("/auto-complete")
     public ResponseEntity<?> autoComplete(@RequestParam String keyword) {
         return null;
@@ -16,9 +23,24 @@ public class CompanyController {
         return null;
     }
 
+    /**
+     * 회사 및 배당금 정보 추가
+     * @param request
+     * @return
+     */
     @PostMapping
-    public ResponseEntity<?> addCompany() {
-        return null;
+    public ResponseEntity<?> addCompany(@RequestBody Company request) {
+        // 사용자가 입력한 ticker 값
+        String ticker = request.getTicker().trim();
+
+        // 클라이언트가 공백을 입력했을 경우
+        if (ObjectUtils.isEmpty(ticker)) {
+            throw new RuntimeException("ticker is empty");
+        }
+
+        Company company = this.companyService.save(ticker);
+
+        return ResponseEntity.ok(company);
     }
 
     @DeleteMapping
