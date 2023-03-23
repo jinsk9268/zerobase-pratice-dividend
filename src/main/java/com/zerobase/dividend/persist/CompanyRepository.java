35380147697635d60058681081831e36fd5c3768
jinsk9268 <jinsk9268@gmail.com>
2023-03-23 13:56:53 +0900

    feat: CompanyRepository-LIKE문을 이용한 회사 이름 찾기 쿼리 추가
    - findByNameStartingWithIgnoreCase

--- src/main/java/com/zerobase/dividend/persist/CompanyRepository.java
+++ src/main/java/com/zerobase/dividend/persist/CompanyRepository.java
@@ -1,6 +1,8 @@
 package com.zerobase.dividend.persist;
 
 import com.zerobase.dividend.persist.entity.CompanyEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,4 +14,7 @@ public interface CompanyRepository extends JpaRepository<CompanyEntity, Long> {
 
     // Optional로 감싸는 이유는 null point exception 방지 및 값이 없을때도 처리하기 편하기 때문
     Optional<CompanyEntity> findByName(String companyName);
+
+    // LIKE - 회사명 기준으로 특정 키워드로 시작하는 키워드를 찾을거고 대소문자 구분은 무시
+    Page<CompanyEntity> findByNameStartingWithIgnoreCase(String s, Pageable pageable);
 }
