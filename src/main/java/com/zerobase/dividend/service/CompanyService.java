d7343d7839380626e904e18106cfbe7239a8696c
jinsk9268 <jinsk9268@gmail.com>
2023-03-23 09:40:37 +0900

    feat: CompanyService-모든 회사 조회 서비스 pageable 기능 추가
    - getAllCompany

--- src/main/java/com/zerobase/dividend/service/CompanyService.java
+++ src/main/java/com/zerobase/dividend/service/CompanyService.java
@@ -8,6 +8,8 @@ import com.zerobase.dividend.persist.entity.CompanyEntity;
 import com.zerobase.dividend.persist.entity.DividendEntity;
 import com.zerobase.dividend.scraper.Scraper;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 
@@ -57,7 +59,7 @@ public class CompanyService {
         return company;
     }
 
-    public List<CompanyEntity> getAllCompany() {
-        return this.companyRepository.findAll();
+    public Page<CompanyEntity> getAllCompany(Pageable pageable) {
+        return this.companyRepository.findAll(pageable);
     }
 }
