7ca2365750c13750b11ff13f91a0d4e195d6bb09
jinsk9268 <jinsk9268@gmail.com>
2023-03-23 10:17:14 +0900

    feat: CompanyRepository-회사 이름으로 회사 찾기 쿼리 메서드 추가
    - findByName

--- src/main/java/com/zerobase/dividend/persist/CompanyRepository.java
+++ src/main/java/com/zerobase/dividend/persist/CompanyRepository.java
@@ -4,7 +4,12 @@ import com.zerobase.dividend.persist.entity.CompanyEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CompanyRepository extends JpaRepository<CompanyEntity, Long> {
     boolean existsByTicker(String ticker);
+
+    // Optional로 감싸는 이유는 null point exception 방지 및 값이 없을때도 처리하기 편하기 때문
+    Optional<CompanyEntity> findByName(String companyName);
 }
