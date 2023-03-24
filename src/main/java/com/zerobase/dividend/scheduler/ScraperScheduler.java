cf645a33952b9f46494c0357cadc61d126aeb083
jinsk9268 <jinsk9268@gmail.com>
2023-03-24 11:03:49 +0900

    feat: ScraperScheduler-스케줄러 메서드-스크래핑한 배당금 정보 중 DB에 없는 값만 저장하도록 구현
    - yahooFinanceScheduling

--- src/main/java/com/zerobase/dividend/scheduler/ScraperScheduler.java
+++ src/main/java/com/zerobase/dividend/scheduler/ScraperScheduler.java
@@ -3,7 +3,9 @@ package com.zerobase.dividend.scheduler;
 import com.zerobase.dividend.model.Company;
 import com.zerobase.dividend.model.ScrapedResult;
 import com.zerobase.dividend.persist.CompanyRepository;
+import com.zerobase.dividend.persist.DividendRepository;
 import com.zerobase.dividend.persist.entity.CompanyEntity;
+import com.zerobase.dividend.persist.entity.DividendEntity;
 import com.zerobase.dividend.scraper.Scraper;
 import lombok.AllArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -15,6 +17,7 @@ import java.util.List;
 @AllArgsConstructor // repository가 초기화 될 수 있도록
 public class ScraperScheduler {
     private final CompanyRepository companyRepository;
+    private final DividendRepository dividendRepository; // 배당금 정보 저장 위해
     private final Scraper yahooFinanceScrapper;
 
     // 일정 주기마다 수행
@@ -31,8 +34,18 @@ public class ScraperScheduler {
                             .ticker(company.getTicker())
                             .build()
             );
-        }
 
-        // 스크래핑한 배당금 정보 중 데이터베이스에 없는 값만 저장
+            // 스크래핑한 배당금 정보 중 데이터베이스에 없는 값만 저장
+            scrapedResult.getDividends().stream()
+                    // dividend 모델을 dividend Entity로 매핑
+                    .map(e -> new DividendEntity(company.getId(), e))
+                    // DividendEntity를 하나씩 DividendRepository에 삽입, 단 존재하지 않는 값만
+                    .forEach(e -> {
+                        boolean exists = this.dividendRepository.existsByCompanyIdAndDate(e.getCompanyId(), e.getDate());
+                        if (!exists) {
+                            this.dividendRepository.save(e);
+                        }
+                    });
+        }
     }
 }
