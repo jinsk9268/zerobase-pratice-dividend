3006838a338e662c73ad1c0fdf653033800853ce
jinsk9268 <jinsk9268@gmail.com>
2023-03-24 14:45:18 +0900

    feat: 프로젝트에서 캐쉬 사용할 수 있도록 어노테이션 추가

--- src/main/java/com/zerobase/dividend/DividendApplication.java
+++ src/main/java/com/zerobase/dividend/DividendApplication.java
@@ -2,10 +2,12 @@ package com.zerobase.dividend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling // 스케쥴링 사용할 수 있도록
+@EnableCaching // 캐시 사용할 수 있도록
 public class DividendApplication {
 
     public static void main(String[] args) {
