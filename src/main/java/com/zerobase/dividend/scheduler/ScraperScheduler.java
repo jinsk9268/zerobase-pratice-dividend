6e9baaba1a92f0e09201b59ad3e9d6b36b7b848a
jinsk9268 <jinsk9268@gmail.com>
2023-03-24 10:45:25 +0900

    feat: ScraperScheduler-스케줄러 생성 및 스케줄러 메서드-저장된 모든 회사 목록 조회 기능 구현
    - yahooFinanceScheduling

---
+++ src/main/java/com/zerobase/dividend/scheduler/ScraperScheduler.java
@@ -0,0 +1,26 @@
+package com.zerobase.dividend.scheduler;
+
+import com.zerobase.dividend.persist.CompanyRepository;
+import com.zerobase.dividend.persist.entity.CompanyEntity;
+import lombok.AllArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@AllArgsConstructor // repository가 초기화 될 수 있도록
+public class ScraperScheduler {
+    private final CompanyRepository companyRepository;
+
+    // 일정 주기마다 수행
+    @Scheduled(cron = "")
+    public void yahooFinanceScheduling() {
+        // 저장된 모든 회사 목록을 조회
+        List< CompanyEntity> companies = this.companyRepository.findAll();
+
+        // 회사 마다 배당금 정보를 스크래핑
+
+        // 스크래핑한 배당금 정보 중 데이터베이스에 없는 값만 저장
+    }
+}
