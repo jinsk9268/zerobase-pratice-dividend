9572dcb5f18e3298414fd23dc21994c5096354a1
jinsk9268 <jinsk9268@gmail.com>
2023-03-24 18:11:56 +0900

    feat: FinanceService-회사명으로 배당금 정보 가져오기에 로깅 추가
    - getDividendByCompanyName

--- src/main/java/com/zerobase/dividend/service/FinanceService.java
+++ src/main/java/com/zerobase/dividend/service/FinanceService.java
@@ -8,6 +8,7 @@ import com.zerobase.dividend.persist.DividendRepository;
 import com.zerobase.dividend.persist.entity.CompanyEntity;
 import com.zerobase.dividend.persist.entity.DividendEntity;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +16,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@Slf4j
 @AllArgsConstructor
 public class FinanceService {
     private final CompanyRepository companyRepository;
@@ -25,8 +27,12 @@ public class FinanceService {
     // 캐시에 사용해도 적합하구나!
     // redis 서버와의 key, value와 의미가 다르니 주의
     // key : 이 메소드의 파라미터명
+    // 캐시에 데이터가 없을경우 로직 실행해서 리턴값 캐시에 추가해주고
+    // 캐시에 데이터가 있는 경우 로직을 실행하지 않고 캐시에 있는 데이터 반환
     @Cacheable(key = "#companyName", value = "finance")
     public ScrapedResult getDividendByCompanyName(String companyName) {
+        log.info("search company -> " + companyName);
+
         // 1. 회사명을 기준으로 회사 정보를 조회
         // orElseThrow - 원래는 Optional<CompanyEntity>로 반환받아야 하나
         // 값이 없으면 인자로 넘겨주는 값을 보여주고 있으면 Optional이 벗겨진 알맹이 그대로 보여줘
