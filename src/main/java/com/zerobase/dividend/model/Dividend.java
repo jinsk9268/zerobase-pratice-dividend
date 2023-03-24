1c7f4998434a33c7c349e56f18da59cde3b1d832
jinsk9268 <jinsk9268@gmail.com>
2023-03-24 16:54:51 +0900

    feat: Dividend-LocalDateTime 레디스 캐시 사용시 직렬화, 역직렬화 설정 추가

--- src/main/java/com/zerobase/dividend/model/Dividend.java
+++ src/main/java/com/zerobase/dividend/model/Dividend.java
@@ -1,5 +1,9 @@
 package com.zerobase.dividend.model;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import lombok.Builder;
 import lombok.Data;
 
@@ -8,6 +12,8 @@ import java.time.LocalDateTime;
 @Data
 @Builder
 public class Dividend {
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     private LocalDateTime date;
     private String dividend;
 }
