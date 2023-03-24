b92cc36b1f742dcfb91f6d67014278d30287f68f
jinsk9268 <jinsk9268@gmail.com>
2023-03-24 10:51:38 +0900

    feat: ScraperScheduler-스케줄러 메서드-회사마다 배당금 정보를 스크래핑할 수 있도록 구현
    - yahooFinanceScheduling

--- src/main/java/com/zerobase/dividend/scheduler/ScraperScheduler.java
+++ src/main/java/com/zerobase/dividend/scheduler/ScraperScheduler.java
@@ -1,7 +1,10 @@
 package com.zerobase.dividend.scheduler;
 
+import com.zerobase.dividend.model.Company;
+import com.zerobase.dividend.model.ScrapedResult;
 import com.zerobase.dividend.persist.CompanyRepository;
 import com.zerobase.dividend.persist.entity.CompanyEntity;
+import com.zerobase.dividend.scraper.Scraper;
 import lombok.AllArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -12,14 +15,23 @@ import java.util.List;
 @AllArgsConstructor // repository가 초기화 될 수 있도록
 public class ScraperScheduler {
     private final CompanyRepository companyRepository;
+    private final Scraper yahooFinanceScrapper;
 
     // 일정 주기마다 수행
     @Scheduled(cron = "")
     public void yahooFinanceScheduling() {
         // 저장된 모든 회사 목록을 조회
-        List< CompanyEntity> companies = this.companyRepository.findAll();
+        List<CompanyEntity> companies = this.companyRepository.findAll();
 
         // 회사 마다 배당금 정보를 스크래핑
+        for (CompanyEntity company : companies) { // var로 선언해도됨
+            ScrapedResult scrapedResult = this.yahooFinanceScrapper.scrap(
+                    Company.builder()
+                            .name(company.getName())
+                            .ticker(company.getTicker())
+                            .build()
+            );
+        }
 
         // 스크래핑한 배당금 정보 중 데이터베이스에 없는 값만 저장
     }
