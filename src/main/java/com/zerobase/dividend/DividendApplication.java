3e734095179d5250fb9b153843564e7a1702f317
jinsk9268 <jinsk9268@gmail.com>
2023-03-24 10:38:19 +0900

    feat: 프로젝트에 Scheduling 사용할 수 있도록 어노테이션 추가

--- src/main/java/com/zerobase/dividend/DividendApplication.java
+++ src/main/java/com/zerobase/dividend/DividendApplication.java
@@ -2,8 +2,10 @@ package com.zerobase.dividend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling // 스케쥴링 사용할 수 있도록
 public class DividendApplication {
 
     public static void main(String[] args) {
