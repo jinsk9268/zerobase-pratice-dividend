2f69fc142118465a2cd5a05d2662db37d4f4fce7
jinsk9268 <jinsk9268@gmail.com>
2023-03-24 17:09:47 +0900

    refactor: Dividend-Builder 어노테이션 삭제

--- src/main/java/com/zerobase/dividend/model/Dividend.java
+++ src/main/java/com/zerobase/dividend/model/Dividend.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -14,7 +13,6 @@ import java.time.LocalDateTime;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 public class Dividend {
     @JsonSerialize(using = LocalDateTimeSerializer.class)
     @JsonDeserialize(using = LocalDateTimeDeserializer.class)
