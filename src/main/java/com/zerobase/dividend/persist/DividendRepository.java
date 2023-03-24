16e9a63566f3dcd198cd1e21ca5983af70c16241
jinsk9268 <jinsk9268@gmail.com>
2023-03-24 10:55:47 +0900

    feat: DividendRepository-해당 날짜와 회사 존재여부 확인 쿼리 메서드 추가
    - existsByCompanyIdAndDate

--- src/main/java/com/zerobase/dividend/persist/DividendRepository.java
+++ src/main/java/com/zerobase/dividend/persist/DividendRepository.java
@@ -4,9 +4,15 @@ import com.zerobase.dividend.persist.entity.DividendEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface DividendRepository extends JpaRepository<DividendEntity, Long> {
     List<DividendEntity> findAllByCompanyId(Long companyId);
+
+    // companyId, date 복합 유닉키 설정해둬서 일반 select where 하는거보다 빠름
+    // 인덱스를 걸지않고 데이터를 조회하게되면 db 성능에 지장이 있음
+    // 해당 날짜와 회사가 존재하는지 확인
+    boolean existsByCompanyIdAndDate(Long companyId, LocalDateTime date);
 }
