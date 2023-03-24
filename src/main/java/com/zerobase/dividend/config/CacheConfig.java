b343ec7427f3d40d6351b699c3d753f2bea0420c
jinsk9268 <jinsk9268@gmail.com>
2023-03-24 16:01:46 +0900

    feat: CacheConfig-레디스 캐시 매니저 메서드 구현
    - redisCacheManager

--- src/main/java/com/zerobase/dividend/config/CacheConfig.java
+++ src/main/java/com/zerobase/dividend/config/CacheConfig.java
@@ -2,11 +2,17 @@ package com.zerobase.dividend.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 @RequiredArgsConstructor
@@ -16,6 +22,20 @@ public class CacheConfig {
 
     @Value("${spring.redis.port}")
     private int port;
+
+    @Bean
+    public CacheManager redisCacheManager(RedisConnectionFactory redisConnectionFactory) {
+        // 직렬화 진행
+        RedisCacheConfiguration conf = RedisCacheConfiguration.defaultCacheConfig()
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
+
+        // RedisCacheManager 리턴, 커넥션은 파라미터로 받은 redisConnectionFactory, 캐시 디폴트는 위에서 생성한 conf
+        return RedisCacheManager.RedisCacheManagerBuilder
+                .fromConnectionFactory(redisConnectionFactory)
+                .cacheDefaults(conf)
+                .build();
+    }
 
     // 레디스 서버와의 연결, 커넥션 관리를 위한 bean 초기화
     // redis와 커넥션 맺을 수 있는 커넥션팩토리 생성
