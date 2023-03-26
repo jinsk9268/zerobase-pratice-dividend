6d8ef3cfab4bf2460790004e5b9bd73309626c0c
jinsk9268 <jinsk9268@gmail.com>
2023-03-26 11:36:31 +0900

    feat: TokenProvider-토큰 생성 메서드 구현
    - generateToken

---
+++ src/main/java/com/zerobase/dividend/security/TokenProvider.java
@@ -0,0 +1,47 @@
+package com.zerobase.dividend.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class TokenProvider {
+    private static final String KEY_ROLES = "roles";
+    private static final long TOKEN_EXPIRE_TIME = 1000 * 60 * 60; // 1 hour
+
+    @Value("${spring.jwt.secret}")
+    private String secretKey;
+
+    /**
+     * 토큰 생성(발급)
+     * @param username
+     * @param roles
+     * @return
+     */
+    public String generateToken(String username, List<String> roles) {
+        // 사용자의 권한 정보 저장
+        Claims claims = Jwts.claims().setSubject(username);
+        claims.put(KEY_ROLES, roles);
+
+        // 토큰이 생성된 시간
+        Date now = new Date();
+
+        // 토큰 만료 시간, var로 선언해도 됨
+        Date expiredDate = new Date(now.getTime() + TOKEN_EXPIRE_TIME);
+
+        // 위의 정보들로 토큰 생성
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now) // 토큰 생성 시간
+                .setExpiration(expiredDate) // 토큰 만료 시간
+                .signWith(SignatureAlgorithm.HS512, this.secretKey) // 사용할 암호화 알고리즘, 비밀키
+                .compact(); // 스트링 변환
+    }
+}
