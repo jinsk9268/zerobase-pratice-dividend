0737024a782a8cfd73d23af713a1a066ee84a90d
jinsk9268 <jinsk9268@gmail.com>
2023-03-26 11:15:25 +0900

    feat: MemberService-세팅 및 회원가입 기능 구현
    - register (회원가입 기능 구현)
    - loadUserByUsername

---
+++ src/main/java/com/zerobase/dividend/service/MemberService.java
@@ -0,0 +1,58 @@
+package com.zerobase.dividend.service;
+
+import com.zerobase.dividend.model.Auth;
+import com.zerobase.dividend.persist.MemberRepository;
+import com.zerobase.dividend.persist.entity.MemberEntity;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@AllArgsConstructor
+public class MemberService implements UserDetailsService {
+    private MemberRepository memberRepository;
+    private PasswordEncoder passwordEncoder; // 비밀번호 암호화를 위해
+
+    // 스프링 시큐리티의 기능을 사용하기 위해
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        // MemberEntity는 UserDetail을 상속해서 만든 클래스이기 때문에 바로 리턴 가능
+        return this.memberRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("couldn't find user -> " + username));
+    }
+
+    // 회원 가입 기능 구현
+    /*
+        1. MemberEntity 먼저 정의, 스프링 시큐리티 사용을 위해 UserDetails 상속
+        2. MemberEntity 사용을 위한 MemberRepository 에 필요한 메서드 정의
+        3. AppConfig 클래스에서 빈 등록, 사용자 메타정보를 저장할때 패스워드 같은 민감정보는 그대로 저장되지 않도록 엔코더 설정
+        4. MemberService 에서 UserDetailsService 를 상속하는 register 메서드 생성
+        5. 파라미터로 받은 id 값이 중복된 아이디인지 확인해주고
+        6. 중복이 없다면 받은 패스워드는 인코딩해서 디비에 저장
+     */
+    public MemberEntity register(Auth.SignUp member) {
+        boolean exists = this.memberRepository.existsByUsername(member.getUsername());
+
+        if (exists) {
+            throw new RuntimeException("이미 사용중인 아이디 입니다.");
+        }
+
+        // 인코딩된 password로 password 재설정
+        member.setPassword(this.passwordEncoder.encode(member.getPassword()));
+
+        // 회원정보 저장
+        MemberEntity result = this.memberRepository.save(member.toEntity());
+
+        return result;
+    }
+
+    // 로그인
+    public MemberEntity authenticate(Auth.SignIn member) {
+        return null;
+    }
+}
