f36e4cfc673bebaf7c90775656c18f80be7c67de
jinsk9268 <jinsk9268@gmail.com>
2023-03-26 17:32:35 +0900

    feat: DividendRepository-companyId로 회사의 배당금 정보 삭제하는 쿼리 메서드 추가
    - deleteAllByCompanyId

--- src/main/java/com/zerobase/dividend/persist/DividendRepository.java
+++ src/main/java/com/zerobase/dividend/persist/DividendRepository.java
@@ -3,6 +3,7 @@ package com.zerobase.dividend.persist;
 import com.zerobase.dividend.persist.entity.DividendEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -10,6 +11,9 @@ import java.util.List;
 @Repository
 public interface DividendRepository extends JpaRepository<DividendEntity, Long> {
     List<DividendEntity> findAllByCompanyId(Long companyId);
+
+    @Transactional
+    void deleteAllByCompanyId(Long id);
 
     // companyId, date 복합 유닉키 설정해둬서 일반 select where 하는거보다 빠름
     // 인덱스를 걸지않고 데이터를 조회하게되면 db 성능에 지장이 있음
